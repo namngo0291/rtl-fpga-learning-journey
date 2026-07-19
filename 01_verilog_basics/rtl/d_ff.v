48be40d0a5e3df73abd0af36d8f80aaf5012d746
namngo0291 <namngo0291@users.noreply.github.com>
2026-07-19 21:32:59 +0700

    Create d_ff.v

---
+++ 01_verilog_basics/rtl/d_ff.v
@@ -0,0 +1,15 @@
+module d_ff (
+    input  wire clk,
+    input  wire rst_n,
+    input  wire d,
+    output reg  q
+);
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n)
+        q <= 1'b0;
+    else
+        q <= d;
+end
+
+endmodule
